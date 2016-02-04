0042d590faba1d2efed044cca2550f5c9275bc94
casa3d <casa3d@outlook.com>
2016-02-03 20:52:59 -0500

    2016.2.3 progressBar & script Attributes
    2016.2.3 progressBar & script Attributes (like
    <script SEARCHING_SERVER LAYOUT_DESIGN COLORS>) added

--- mibuscadordevuelos/INICIAR_APLI_DE_VUELOS.js
+++ mibuscadordevuelos/INICIAR_APLI_DE_VUELOS.js
@@ -1,29 +1,33 @@
 i={//Ιοτα
- o:['../Estilos/Ssoft/UgaStyles.css'],
+ o:['../Estilos/Ssoft/proBar.css'],
  d:document,
  //http://mibuscadordevuelos.appspot.com/
  hrf:'WorldTours/Pagina/App_Themes/Scripts/',
  dir:'jQuery ../Estilos/jQuery/jsTurinco Ssoft Hoteles Optimizacion Conbinaciones'.split(' '),
  js:(//async since pos3
-  'jquery.min-1.11.1 sessvars easySlider1.5 jquery.fadetransition jquery.featureList-1.0.0 jquery.wslide jquery.skinned-select peloslideli.v1.0.1.min jquery.autocomplete jquery.cookies.2.1.0 swfobject jquery.dataTables jquery.jcarousel.min jquery-ui jquery.validationEngine jquery.validationEngine-es:'+
+  'layout jquery.min-1.11.1 sessvars easySlider1.5 jquery.fadetransition jquery.featureList-1.0.0 jquery.wslide jquery.skinned-select peloslideli.v1.0.1.min jquery.autocomplete jquery.cookies.2.1.0 swfobject jquery.dataTables jquery.jcarousel.min jquery-ui jquery.validationEngine jquery.validationEngine-es:'+
   'jquery.idletimeout jquery.idletimer:'+
   'ValidadorTarjeta Funciones Estilo ModalMensaje:'+
   'Hoteles:'+
   'search setDate:'+
   'VueloHotel/searchVueloHotel Paquetes/widget_package').split(':'),
- lay:function(x){
-  (x=new XMLHttpRequest).send(x.open('GET',i.hrf+'Ssoft/layout.js'))
-  x.onload=Function('ssoft_white_label.innerHTML=this.response,i.url()')},
  nw:function(t,e){//tag, new Element
   i.d.head.appendChild(e=i.d.createElement(t>0?'link':'script'))[t>0?(e.rel='stylesheet','href'):'src']=i.hrf+i.o.shift()
-  i.o[0]&&(e.onload=i.nw)},
+  i.PB()//i.pb,i.o.shift()
+  i.o[0]?
+   e.onload=i.nw
+   //i.nw()
+   :i.pb.classList.remove('pro-bar')},
+ PB:function(){//ProgressBar
+  i.pb?
+   i.pb.setAttribute('style','width:'+(270/30*(30-i.o.length))+'px!important')
+   :i.pb=ssoft_white_label.getElementsByClassName('ui-tabs-nav')[0]},
  url:function(o,d,j,p){//urlsObj, parentDirectory[d], file.js[j], position[p]
-  i.nw(1)
   for(d in i.dir){
    o=i.js[d].split(' ')
    for(j in o)i.o.push(o[j]=i.dir[d]+'/'+o[j]+'.js')}
-  i.nw()}}
+  i.nw(1)}}
 
 i.d.readyState=='complete'?
- i.lay()
- :onload=i.lay
+ i.url()
+ :onload=i.url
